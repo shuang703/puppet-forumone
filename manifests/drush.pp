b56d8006d2ed895699082e4b9fe5baa8e43cba99
William Hurley <whurley@forumone.com>
2014-02-19 19:12:13 -0500

    Fixing require on extraction

--- manifests/drush.pp
+++ manifests/drush.pp
@@ -5,7 +5,6 @@ class forumone::drush ($version = '7.x-5.9') {
   exec { 'forumone::drush::download':
     command => "wget --directory-prefix=/opt http://ftp.drupal.org/files/projects/${filename}",
     path    => '/usr/bin',
-    require => Package["java-1.7.0-openjdk"],
     creates => "/opt/${filename}",
     timeout => 4800,
   }
@@ -14,7 +13,7 @@ class forumone::drush ($version = '7.x-5.9') {
   exec { 'forumone::drush::extract':
     command => "tar -zxvf /opt/${filename} -C /opt",
     path    => ["/bin"],
-    require => [Exec["forumone::solr::download"]],
+    require => Exec["forumone::drush::download"],
     creates => '/opt/drush/LICENSE.txt',
   }
 
