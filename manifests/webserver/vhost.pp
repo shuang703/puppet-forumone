5f8064233b43a38ea98ee63603c139be343c0968
Campbell Vertesi <campbell@vertesi.com>
2014-12-04 19:18:07 +0100

    minor tweaks on aliases declaration... just casting around at this point.

--- manifests/webserver/vhost.pp
+++ manifests/webserver/vhost.pp
@@ -11,10 +11,10 @@ define forumone::webserver::vhost (
     if $::forumone::webserver::webserver == 'apache' {
       apache::vhost { $name:
         servername    => $servername,
-        aliases { $aliases:
+        aliases [{ $aliases:
           alias     => $name,
           path      => $destination
-        }
+        }]
         port          => $::forumone::webserver::port,
         docroot       => $path,
         docroot_group => $::host_gid,
