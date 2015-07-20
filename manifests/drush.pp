bd9d97e4db053b1dfbd9514facf80583a6d0915f
William Hurley <whurley@forumone.com>
2015-07-20 13:58:09 -0400

    Fixing typo on user

--- manifests/drush.pp
+++ manifests/drush.pp
@@ -46,6 +46,6 @@ class forumone::drush ($version = '7.0.0') {
     creates => "/opt/drush-${version}/vendor/bin/phpunit",
     require => [Exec["forumone::drush::extract"], Class['forumone::composer']],
     environment => ["COMPOSER_HOME=${::forumone::composer::home}"],
-    user    => ::forumone::composer::user,
+    user    => $::forumone::composer::user,
   }
 }
