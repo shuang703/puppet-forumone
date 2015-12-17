41084f928d5c8c9bb7f7d2de53855c5daa5c2190
William Hurley <whurley@forumone.com>
2015-12-16 19:20:42 -0500

    Setting default to 0.12.9

--- manifests/nodejs.pp
+++ manifests/nodejs.pp
@@ -1,2 +1,12 @@
-class forumone::nodejs ($modules = ["grunt-cli"]) {
+class forumone::nodejs ($modules = ["grunt-cli"], $version = 'v0.12.9') {
+  class { '::nodejs':
+    version	 => $version,
+    make_install => false,
+  }
+
+  package { $modules:
+    ensure   => present,
+    provider => 'npm',
+    require  => Class['::nodejs']
+  }
 }
