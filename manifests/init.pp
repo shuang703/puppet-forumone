be1beec0e75287f32f4b10739001e6f1a79d214a
Daniel McDermott <dmcdermott@forumone.com>
2016-03-07 11:59:55 -0800

    Adding Behat Functionality to Webstarter

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class forumone ($ports = [80, 443, 8080, 8081, 18983, 8983, 3306, 13306, 1080],) {
+class forumone ($ports = [80, 443, 8080, 8081, 18983, 8983, 3306, 13306, 1080, 4444, 5555],) {
   case $::operatingsystem {
     /(?i:redhat|centos)/ : {
       class { 'forumone::os::fedora::project': }
