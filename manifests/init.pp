ea1690f081498b406b2f84f7268a7f53672680c2
William Hurley <william@saplingstudio.com>
2016-04-13 17:57:36 -0400

    Install Ruby from pre-compiled binaries (#31)
    * Moving vagrant cachier directory to init.pp to make it globally accessible
    * Changing to use pre-built binaries
    * Removing unnecessary bundler steps

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,11 @@ class forumone ($ports = [80, 443, 8080, 8081, 18983, 8983, 3306, 13306, 1080, 4
       class { 'forumone::os::fedora::project': }
     }
   }
+  
+  # Vagrant cachier directory
+  file { "/tmp/vagrant-cache":
+    ensure  => "directory"
+  }
 
   file { "/home/vagrant/.bashrc":
     ensure  => present,
